23baa935c135ecd7cc0115d6e476e43e5b84ddc5
Aman Kumar Sinha <amankrsinha07@gmail.com>
2023-11-08 18:56:31 +0530

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import CourseContent from "../components/CourseContent";
 
 function App() {
   return (
     <>
       <div className="container">
         <h1>React Js Full Course</h1>
+        <CourseContent />
       </div>
     </>
   );
