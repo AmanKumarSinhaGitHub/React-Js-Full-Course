32c21bcd95e609e62dafa79ff2f8d01cef07e1aa
Aman Kumar Sinha <amankrsinha07@gmail.com>
2023-11-08 18:46:19 +0530

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container">
+        <h1>React Js Full Course</h1>
       </div>
-      <h1>React Js Full Course</h1>
-     
     </>
-  )
+  );
 }
 
-export default App
+export default App;
