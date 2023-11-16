95ef1c962ee179f0c1b8dece5756e99305cbf4f4
Aman Kumar Sinha <amankrsinha07@gmail.com>
2023-11-16 15:37:22 +0530

    Project-SetUp

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import "./App.css";
-import CourseContent from "../components/CourseContent";
 
 function App() {
   return (
     <>
       <div className="container">
-        <h1>React Js Full Course</h1>
-        <CourseContent />
+        <h1>Digital Clock</h1>
       </div>
     </>
   );
